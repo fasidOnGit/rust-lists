97f6afaadd19e5082ff0b4b625f8bc1b466b5653
Kader Fasid <fasidmpm@gmail.com>
2020-05-09 14:44:46 +0530

    peek and peek mut. and tests

--- src/second.rs
+++ src/second.rs
@@ -28,6 +28,14 @@ impl<T> List<T> {
             node.elem
         })
     }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -48,35 +56,20 @@ impl<T> Drop for List<T> {
     }
 }
 
-#[cfg(test)]
 mod test {
-    use crate::first::List;
+    use crate::second::List;
 
     #[test]
-    fn basics() {
+    fn peek() {
         let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1); list.push(2); list.push(3);
 
-        // Check empty list behaves right
-        assert_eq!(list.pop(), None);
-
-        // Populate list
-        list.push(1);
-        list.push(2);
-        list.push(3);
-        // Check normal removal
-        assert_eq!(list.pop(), Some(3));
-        assert_eq!(list.pop(), Some(2));
-
-        // Push some more just to make sure nothing's corrupted
-        list.push(4);
-        list.push(5);
-
-        // Check normal removal
-        assert_eq!(list.pop(), Some(5));
-        assert_eq!(list.pop(), Some(4));
-
-        // Check exhaustion
-        assert_eq!(list.pop(), Some(1));
-        assert_eq!(list.pop(), None);
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+        list.peek_mut().map(|value| { *value = 42 });
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Some(42));
     }
 }
