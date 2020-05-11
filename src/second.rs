46de4e1e46773b13a4cec5aaec19bb9f0124c1eb
Kader Fasid <fasidmpm@gmail.com>
2020-05-11 11:23:28 +0530

    IterMut.... done

--- src/second.rs
+++ src/second.rs
@@ -103,6 +103,36 @@ impl<'a, T> Iterator for Iter<'a, T> {
     }
 }
 
+
+// Iter is generic over *some* lifetime, it doesn't care
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>
+}
+// No lifetime here, List doesn't have any associated lifetimes
+impl<T> List<T> {
+    // We declare a fresh lifetime here for the *exact* borrow that
+    // creates the iter. Now &self needs to be valid as long as the
+    // Iter is around.
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> { //Lifetime ellision...
+        IterMut { next: self.head.as_mut().map(|node| &mut **node)}
+    }
+}
+// We *do* have a lifetime here, because Iter has one that we need to define
+impl<'a, T> Iterator for IterMut<'a, T> {
+    // Need it here too, this is a type declaration
+    type Item = &'a mut T;
+
+    // None of this needs to change, handled by the above.
+    // Self continues to be incredibly hype and amazing
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            // self.next = node.next.as_ref().map(|node| &**node); -> Can also be written as below with the turbofish! `::<&Node<T>, _>`
+            self.next = node.next.as_mut().map(|node| &mut **node);
+            &mut node.elem
+        })
+    }
+}
+
 #[cfg(test)]
 mod test {
     use crate::second::List;
@@ -141,5 +171,16 @@ mod test {
         assert_eq!(iter.next(), Some(&3));
         assert_eq!(iter.next(), Some(&2));
         assert_eq!(iter.next(), Some(&1));
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
     }
 }
